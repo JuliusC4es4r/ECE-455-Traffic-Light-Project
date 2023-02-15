3ec02d159da633e38192971a8bd553880ac09b96
Carter Cooper <cartercooper@uvic.ca>
2023-02-14 18:33:34 -0800

    lab2 - almost done

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "stm32f4_discovery.h"
 
 #include "stm32f4xx.h"
@@ -19,11 +20,16 @@
 
 #define ADC_PIN GPIO_Pin_3
 #define RED_PIN GPIO_Pin_0
-#define AMBER_PIN GPIO_Pin_1
+#define YELLOW_PIN GPIO_Pin_1
 #define GREEN_PIN GPIO_Pin_2
 #define DATA_PIN GPIO_Pin_6
 #define CLOCK_PIN GPIO_Pin_7
 #define RESET_PIN GPIO_Pin_8
+
+#define RED_STATE 0x01
+#define YELLOW_STATE 0x02
+#define GREEN_STATE 0x03
+
 
 
 /*
@@ -35,13 +41,21 @@ static void prvSetupHardware( void );
 static void TrafficFlowAdjustment( void *pvParameters );
 static void TrafficGenerator( void *pvParameters );
 static void LightState( void *pvParameters );
-static void SystemDisplay( void *pvParameters );
+static void SystemDisplay( TimerHandle_t xTimer );
 static void adcInit(void);
 static void gpioInit(void);
-static void timerInit(void);
 static uint16_t getADC(void);
 
-uint32_t shift_output = 0x00000000;
+SemaphoreHandle_t trafficOutputMutex;
+uint32_t trafficOutput = 0x00000000;
+
+SemaphoreHandle_t potValueMutex;
+uint32_t potValue = 0;
+
+SemaphoreHandle_t trafficLightMutex;
+uint8_t trafficLight = RED_STATE;
+
+TimerHandle_t displayTimer;
 
 
 /*-----------------------------------------------------------*/
@@ -54,16 +68,33 @@ int main(void)
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	adcInit();
 	gpioInit();
-	timerInit();
+
+	displayTimer = xTimerCreate(
+			"displayTimer",
+			pdMS_TO_TICKS(1000 / portTICK_RATE_MS),
+			pdTRUE,
+			(void*)0,
+			SystemDisplay
+	);
+
+	xTimerStart(displayTimer, portMAX_DELAY);
+
+	trafficOutputMutex = xSemaphoreCreateMutex();
+	xSemaphoreGive(trafficOutputMutex);
+
+	potValueMutex = xSemaphoreCreateMutex();
+	xSemaphoreGive(potValueMutex);
+
+	trafficLightMutex = xSemaphoreCreateMutex();
+	xSemaphoreGive(trafficLightMutex);
 
 	xTaskCreate( TrafficFlowAdjustment, "Traffic Flow", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 	xTaskCreate( TrafficGenerator, "Traffic Generator", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 	xTaskCreate( LightState, "Traffic Light", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-	xTaskCreate( SystemDisplay, "System Display", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+//	xTaskCreate( SystemDisplay, "System Display", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -106,14 +137,13 @@ static void adcInit(void)
 static void gpioInit(void)
 {
 
-	GPIO_PinAFConfig(GPIOC, CLOCK_PIN, GPIO_AF_TIM3);
-
 	GPIO_InitTypeDef gpioInit =
 	{
 			.GPIO_Pin = RED_PIN |
-						AMBER_PIN |
+						YELLOW_PIN |
 						GREEN_PIN |
 						DATA_PIN |
+						CLOCK_PIN |
 						RESET_PIN,
 			.GPIO_Mode = GPIO_Mode_OUT,
 			.GPIO_Speed = GPIO_Speed_100MHz,
@@ -123,46 +153,6 @@ static void gpioInit(void)
 	};
 
 	GPIO_Init(GPIOC, &gpioInit);
-
-	GPIO_InitTypeDef gpioTimInit =
-	{
-		.GPIO_Pin = CLOCK_PIN,
-		.GPIO_Mode = GPIO_Mode_AF,
-		.GPIO_Speed = GPIO_Speed_100MHz,
-		.GPIO_OType = GPIO_OType_PP,
-		.GPIO_PuPd = GPIO_PuPd_NOPULL
-
-	};
-
-	GPIO_Init(GPIOC, &gpioTimInit);
-
-}
-
-static void timerInit(void)
-{
-	TIM_TimeBaseInitTypeDef timerInit =
-	{
-			.TIM_Prescaler = 128,
-			.TIM_CounterMode = TIM_CounterMode_Up,
-			.TIM_Period = 8400,
-			.TIM_ClockDivision = TIM_CKD_DIV1
-
-	};
-
-	TIM_TimeBaseInit(TIM3, &timerInit);
-	TIM_Cmd(TIM3, ENABLE);
-
-	TIM_OCInitTypeDef tim_oc =
-	{
-		.TIM_OCMode = TIM_OCMode_PWM1,
-		.TIM_OutputState = TIM_OutputState_Enable,
-		.TIM_OCPolarity = TIM_OCPolarity_High,
-		.TIM_Pulse = 2099
-	};
-
-	/* 25% duty cycle */
-	TIM_OC1Init(TIM4, &tim_oc);
-	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
 
 }
 
@@ -184,7 +174,12 @@ static void TrafficFlowAdjustment( void *pvParameters )
 	while(1)
 	{
 		rawADC = getADC();
-		printf("%d\n", rawADC);
+//		printf("%d\n", rawADC);
+
+		xSemaphoreTake(potValueMutex, portMAX_DELAY);
+		potValue = rawADC;
+		xSemaphoreGive(potValueMutex);
+
 		vTaskDelay(500 / portTICK_RATE_MS);
 	}
 }
@@ -195,7 +190,28 @@ static void TrafficGenerator( void *pvParameters )
 {
 	while(1)
 	{
-		vTaskDelay(500 / portTICK_RATE_MS);
+		uint8_t tempState;
+		xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+		tempState = trafficLight;
+		xSemaphoreGive(trafficLightMutex);
+
+		uint16_t tempADC;
+		xSemaphoreTake(potValueMutex, portMAX_DELAY);
+		tempADC = potValue;
+		xSemaphoreGive(potValueMutex);
+
+		int flowRate = (int) ( 6500 - (5.5 * tempADC) );
+		float tempRand = (rand() % 6 ) * 50;
+
+		int generationDelay = (int)flowRate + tempRand;
+
+		printf("%d \n", generationDelay);
+
+		vTaskDelay(generationDelay / portTICK_RATE_MS);
+
+		xSemaphoreTake(trafficOutputMutex, portMAX_DELAY);
+		trafficOutput |= 0x0001;
+		xSemaphoreGive(trafficOutputMutex);
 	}
 }
 
@@ -206,20 +222,131 @@ static void LightState( void *pvParameters )
 {
 	while(1)
 	{
-		GPIO_SetBits(GPIOC, RED_PIN | AMBER_PIN | GREEN_PIN);
-		vTaskDelay(500 / portTICK_RATE_MS);
-		GPIO_ResetBits(GPIOC, RED_PIN | AMBER_PIN | GREEN_PIN);
-		vTaskDelay(500 / portTICK_RATE_MS);
+		uint8_t tempState;
+		xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+		tempState = trafficLight;
+		xSemaphoreGive(trafficLightMutex);
+
+		uint16_t tempADC;
+		xSemaphoreTake(potValueMutex, portMAX_DELAY);
+		tempADC = potValue;
+		xSemaphoreGive(potValueMutex);
+
+		int flowRate = (int) ( 7500 - (5 * tempADC) );
+		int inverseFlowRate = (int) ( 2500 + (5 * tempADC) );
+
+		printf("%d, %d \n", flowRate, inverseFlowRate);
+
+		switch (tempState)
+		{
+			case(RED_STATE):
+
+				GPIO_ResetBits(GPIOC, RED_PIN | YELLOW_PIN | GREEN_PIN);
+				GPIO_SetBits(GPIOC, RED_PIN);
+				vTaskDelay( flowRate / portTICK_RATE_MS);
+
+				xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+				trafficLight = GREEN_STATE;
+				xSemaphoreGive(trafficLightMutex);
+
+				break;
+
+			case(YELLOW_STATE):
+
+				GPIO_ResetBits(GPIOC, RED_PIN | YELLOW_PIN | GREEN_PIN);
+				GPIO_SetBits(GPIOC, YELLOW_PIN);
+
+				vTaskDelay(2000 / portTICK_RATE_MS);
+
+				xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+				trafficLight = RED_STATE;
+				xSemaphoreGive(trafficLightMutex);
+
+				break;
+
+			case(GREEN_STATE):
+
+				GPIO_ResetBits(GPIOC, RED_PIN | YELLOW_PIN | GREEN_PIN);
+				GPIO_SetBits(GPIOC, GREEN_PIN);
+				vTaskDelay(inverseFlowRate / portTICK_RATE_MS);
+
+				xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+				trafficLight = YELLOW_STATE;
+				xSemaphoreGive(trafficLightMutex);
+
+				break;
+
+			default:
+				break;
+		}
 	}
 }
 
 /*-----------------------------------------------------------*/
 
-static void SystemDisplay( void *pvParameters )
+static void SystemDisplay( TimerHandle_t xTimer )
 {
 	while(1)
 	{
-		vTaskDelay(500 / portTICK_RATE_MS);
+		GPIO_ResetBits(GPIOC, RESET_PIN);
+		vTaskDelay(1 / portTICK_RATE_MS);
+		GPIO_SetBits(GPIOC, RESET_PIN);
+
+		uint32_t tempTraffic;
+		xSemaphoreTake(trafficOutputMutex, portMAX_DELAY);
+		tempTraffic = trafficOutput;
+		xSemaphoreGive(trafficOutputMutex);
+
+		for(int i = 0; i < 20; i++)
+		{
+			GPIO_SetBits(GPIOC, CLOCK_PIN);
+
+			if(tempTraffic & (0x00000001 << i))
+			{
+				GPIO_SetBits(GPIOC, DATA_PIN);
+			}
+			else
+			{
+				GPIO_ResetBits(GPIOC, DATA_PIN);
+			}
+
+			GPIO_ResetBits(GPIOC, CLOCK_PIN);
+		}
+		GPIO_ResetBits(GPIOC, DATA_PIN);
+
+		uint8_t tempState;
+		xSemaphoreTake(trafficLightMutex, portMAX_DELAY);
+		tempState = trafficLight;
+		xSemaphoreGive(trafficLightMutex);
+
+		if(tempState == GREEN_STATE)
+		{
+			xSemaphoreTake(trafficOutputMutex, portMAX_DELAY);
+			trafficOutput = trafficOutput << 1;
+			xSemaphoreGive(trafficOutputMutex);
+		}
+		else
+		{
+			xSemaphoreTake(trafficOutputMutex, portMAX_DELAY);
+
+			uint32_t postLight = trafficOutput & 0xFFFFFF00;
+			uint8_t preLight = trafficOutput & 0xFF;
+
+			for(int i = 7; i >= 0; i--)
+			{
+				if( !(preLight & (0x01 << i) ))
+				{
+//					preLight = ((preLight & (((0x00FF << (i-1)) & 0xFF00))) << 1) & (0xFF00 >> (8-i));
+					preLight = ((preLight & ((((0x00FF << (i-1)) & 0xFF00) >> 8 )) << 1) & (0xFF00 >> (8-i)) );
+				}
+			}
+
+			trafficOutput = (postLight << 1) | preLight;
+
+			xSemaphoreGive(trafficOutputMutex);
+		}
+
+		vTaskDelay(1000);
 	}
 }
 
@@ -233,7 +360,7 @@ void vApplicationMallocFailedHook( void )
 
 	Called if a call to pvPortMalloc() fails because there is insufficient
 	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-	internally by FreeRTOS API functions that create tasks, queues, software 
+	internally by FreeRTOS API functions that create tasks, queues, software
 	timers, and semaphores.  The size of the FreeRTOS heap is set by the
 	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
 	for( ;; );
